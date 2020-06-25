b96162c05174b2f3c5a21dcc6bae2e43768a1a8b
royorel <royorel@cs.washington.edu>
2020-06-25 02:10:19 -0700

    added FFHQ-Aging dataset pruning script

---
+++ datasets/create_dataset.py
@@ -0,0 +1,116 @@
+import argparse
+import PIL.Image as Image
+import os
+import csv
+import shutil
+import pickle
+from pdb import set_trace as st
+
+clusters = ['0-2','3-6','7-9','10-14','15-19',
+            '20-29','30-39','40-49','50-69','70-120']
+
+def processIm(img_filename, phase, csv_row):
+    img_basename = os.path.basename(img_filename)
+    labels_filename = os.path.join(os.path.dirname(img_filename), 'parsings', img_basename)
+
+    age, age_conf = csv_row['age_group'], float(csv_row['age_group_confidence'])
+    gender, gender_conf = csv_row['gender'], float(csv_row['gender_confidence'])
+    head_pitch, head_roll, head_yaw = float(csv_row['head_pitch']), float(csv_row['head_roll']), float(csv_row['head_yaw'])
+    left_eye_occluded, right_eye_occluded = float(csv_row['left_eye_occluded']), float(csv_row['right_eye_occluded'])
+    glasses = csv_row['glasses']
+
+    no_attributes_found = head_pitch == -1 and head_roll == -1 and head_yaw == -1 and \
+                          left_eye_occluded == -1 and right_eye_occluded == -1 and glasses == -1
+
+    age_cond = age_conf > 0.6
+    gender_cond = gender_conf > 0.66
+    head_pose_cond = abs(head_pitch) < 30.0 and abs(head_yaw) < 40.0
+    eyes_cond = (left_eye_occluded < 90.0 and right_eye_occluded < 50.0) or (left_eye_occluded < 50.0 and right_eye_occluded < 90.0)
+    glasses_cond = glasses != 'Dark'
+
+    valid1 = age_cond and gender_cond and no_attributes_found
+    valid2 = age_cond and gender_cond and head_pose_cond and eyes_cond and glasses_cond
+
+    if gender == 'male':
+        dst_gender = 'males'
+    else:
+        dst_gender = 'females'
+
+    dst_cluster = phase + age
+
+    if (valid1 or valid2):
+        dst_path = os.path.join(dst_gender, dst_cluster, img_basename)
+        parsing_dst_path = os.path.join(dst_gender, dst_cluster, 'parsings', img_basename)
+        shutil.copy(img_filename, dst_path)
+        shutil.copy(labels_filename, parsing_dst_path)
+
+    with open('counter.pkl','wb') as f:
+        pickle.dump(num,f)
+
+def create_dataset(folder, train_split):
+    # create a dictionary of all images as well as parsings directory
+    imgs = {}
+    img_subdirs = next(os.walk(folder))[1]
+    img_subdirs.sort()
+    file_counter = 0
+    for subdir in img_subdirs:
+        temp_files = os.listdir(os.path.join(folder,subdir))
+        temp_files.sort()
+        files = [f for f in temp_files if os.path.isfile(os.path.join(folder,subdir,f))]
+
+        for f in files:
+            imgs[file_counter] = os.path.join(folder,subdir,f)
+            file_counter += 1
+
+    # that's a small mechanism to remember the last successfully processed images
+    # in case the code stops
+    try:
+        with open('counter.pkl','rb') as f:
+            counter = pickle.load(f)
+    except:
+        counter = -1
+
+    # create all necessary subfolders
+    for clust in clusters:
+        trainMaleClusterPath = "males/train" + clust
+        testMaleClusterPath = "males/test" + clust
+        trainFemaleClusterPath = "females/train" + clust
+        testFemleClusterPath = "females/test" + clust
+
+        if not os.path.isdir(trainMaleClusterPath):
+            os.makedirs(trainMaleClusterPath)
+            os.makedirs(os.path.join(trainMaleClusterPath,'parsings'))
+            os.makedirs(testMaleClusterPath)
+            os.makedirs(os.path.join(testMaleClusterPath,'parsings'))
+            os.makedirs(trainFemaleClusterPath)
+            os.makedirs(os.path.join(trainFemaleClusterPath,'parsings'))
+            os.makedirs(testFemleClusterPath)
+            os.makedirs(os.path.join(testFemleClusterPath,'parsings'))
+
+    # process images
+    labels_file = os.path.join(folder, 'ffhq_labels.csv')
+    with open(labels_file,'r', newline='') as f:
+        reader = csv.DictReader(f)
+        for csv_row in reader:
+            num = int(csv_row['image_number'])
+            if num <= counter:
+                continue
+
+            if num < train_split:
+                phase = 'train'
+            else:
+                phase = 'test'
+
+            img_filename = imgs[num]
+            print('processing {}'.format(img_filename))
+            processIm(img_filename, phase, csv_row)
+
+
+if __name__ == '__main__':
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument('--folder', type=str, default='../../ffhq_aging256x256', help='Location of the raw FFHQ-Aging dataset')
+    argparser.add_argument('--train_split', type=int, default=69000, help='number of images to allocate for training')
+    args = argparser.parse_args()
+    folder = args.folder
+    train_split = args.folder.train_split
+    create_dataset(folder, train_split)
