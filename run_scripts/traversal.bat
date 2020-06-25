e8d1680d13d8574b9d77eca8f6a1ed34c5ae0b80
royorel <royorel@cs.washington.edu>
2020-06-25 04:16:58 -0700

    added windows run scripts

---
+++ run_scripts/traversal.bat
@@ -0,0 +1,5 @@
+@echo off
+
+set CUDA_VISIBLE_DEVICES=0
+
+python test.py --dataroot ../ffhq_aging_new_labels_our_alignment/males --name test_model --which_epoch 400 --sort_order 0-2,3-6,7-9,15-19,30-39,50-69 --display_id 0 --conv_weight_norm --use_modulated_conv --normalize_mlp --traverse --interp_step 0.05 --image_path_file males_image_list.txt --make_video --verbose
