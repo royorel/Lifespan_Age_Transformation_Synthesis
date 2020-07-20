b992ca8b82e9eecc3681db162c4e844d432013bc
royorel <royorel@cs.washington.edu>
2020-07-20 04:25:03 -0700

    removed fgnet scripts

--- run_scripts/eval_fgnet.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-set CUDA_VISIBLE_DEVICES=0
-
-python test_fgnet.py --fgnet --dataroot ../FGNET_with_nvidia_alignment/males --name test_model --which_epoch 400 --how_many 100 --display_id 0 --verbose
