fd473f8b991fc303f2fcc96a2b3f3bc29e4bd77c
Willy Angole <103872831+Willy-Angole@users.noreply.github.com>
2023-01-12 10:11:38 -0500

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,5 +1,3 @@
-### Copyright (C) 2020 Roy Or-El. All rights reserved.
-### Licensed under the CC BY-NC-SA 4.0 license (https://creativecommons.org/licenses/by-nc-sa/4.0/legalcode).
 import os
 import torch
 import torch.nn as nn
